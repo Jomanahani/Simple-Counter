e5b3ef42b6f5e6afd4bece1ba338d4e047f910ad
Jomanahani <jomanahani123456@gmail.com>
2022-12-23 15:28:11 +0200

    create counter

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Counters from './Component/Counters/index'
+import './index.css'
+  export default class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <Counters/>
+    </div>
+    )
+  }
+}
---
+++ src/Component/Counters/index.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import Counter from '../Counter'
+
+class Counters extends Component {
+    state ={
+        counters:[ 
+            {id:0,count: 0,steps:2},
+            {id:1,count: 0,steps:3},
+            {id:2,count: 0,steps:5},
+          ]   
+    }
+
+    onIncrement = (id, steps = 1) => {
+      this.setState(prevState => ({total: prevState.total + steps}))
+      this.setState((prevState) => {
+        return {
+          counters: prevState.counters.map((item) => {
+            if (item.id === id) {
+              return { ...item, count: item.count + steps };
+            }
+            return item;
+          }),
+        };
+      });
+    };
+  
+    onDecrement = (id, steps = 1) => {
+      let isDec=false
+      this.setState((prevState) => {
+        return {
+          counters: prevState.counters.map((item) => {
+            if (item.id === id && item.count >= steps) {
+              isDec=true
+              return { ...item, count: item.count - steps };
+            }
+            return item;
+          }),
+          
+          total:isDec?prevState.total - steps:prevState.total
+        };
+        
+      });
+    };
+
+  render() {
+    return (
+      <div>
+        {this.state.counters.map((counter)=> 
+        <Counter key={counter.id} count={counter.count} onIncrement={this.onIncrement} onDecrement={this.onDecrement} />)}
+        <p><h2>Total Counter :</h2> </p>
+      </div>
+    )
+  }
+}
+export default Counters;
