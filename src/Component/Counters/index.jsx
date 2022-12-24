06f5c68d8a64b0a082ac0a4f93cba923da5bfc21
Jomanahani <jomanahani123456@gmail.com>
2022-12-24 21:51:06 +0200

    edite counter

--- src/Component/Counters/index.jsx
+++ src/Component/Counters/index.jsx
@@ -4,39 +4,40 @@ import Counter from '../Counter'
 class Counters extends Component {
     state ={
         counters:[ 
-            {id:0,count: 0,steps:2},
-            {id:1,count: 0,steps:3},
-            {id:2,count: 0,steps:5},
+            {id:0,count:0,steps:2},
+            {id:1,count:0,steps:3},
+            {id:2,count:0,steps:5},
           ]   
     }
 
-    onIncrement = (id, steps = 1) => {
-      this.setState(prevState => ({total: prevState.total + steps}))
+    onIncrement = (id, steps =1) => {
+      // console.log(id)
+       // this.setState(prevState => ({total: prevState.total + steps}))
       this.setState((prevState) => {
         return {
           counters: prevState.counters.map((item) => {
             if (item.id === id) {
-              return { ...item, count: item.count + steps };
+              return { ...item, count: item.count + steps }
             }
-            return item;
-          }),
-        };
-      });
-    };
+            return item
+          })
+        }
+      })
+    }
   
-    onDecrement = (id, steps = 1) => {
-      let isDec=false
+    onDecrement = (id, steps =1) => {
+      // let isDec=false
       this.setState((prevState) => {
         return {
           counters: prevState.counters.map((item) => {
-            if (item.id === id && item.count >= steps) {
-              isDec=true
+            if (item.id === id && item.count >= steps ) {
+              // isDec=true
               return { ...item, count: item.count - steps };
             }
             return item;
           }),
           
-          total:isDec?prevState.total - steps:prevState.total
+          // total:isDec?prevState.total - steps:prevState.total
         };
         
       });
@@ -46,8 +47,10 @@ class Counters extends Component {
     return (
       <div>
         {this.state.counters.map((counter)=> 
-        <Counter key={counter.id} count={counter.count} onIncrement={this.onIncrement} onDecrement={this.onDecrement} />)}
-        <p><h2>Total Counter :</h2> </p>
+        <Counter key={counter.id} count={counter.count} onIncrement={()=>{this.onIncrement(counter.id,counter.steps)}} onDecrement={()=>{this.onDecrement(counter.id,counter.steps)}} />)}
+        {/* <p><h2>Total Counter :</h2> </p> */}
+
+        
       </div>
     )
   }
